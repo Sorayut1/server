a3e6ddd5f0bc931a77bac9b5a1acb76c0411da92
Sorayut123 <ok0804042417@gmail.com>
2025-09-03 04:23:00 +0700

    แก้ไข db.js ใช้ Connection Pool

--- config/db.js
+++ config/db.js
@@ -1,21 +1,15 @@
 const mysql = require('mysql2');
-require('dotenv').config(); // โหลดค่าจาก .env
+require('dotenv').config(); // โหลดค่า .env
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3000
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('❌ การเชื่อมต่อ MySQL ล้มเหลว:', err);
-  } else {
-    console.log('✅ เชื่อมต่อ MySQL สำเร็จ');
-  }
-});
-
-
-module.exports = db;
+module.exports = pool.promise(); // ใช้ async/await ได้
